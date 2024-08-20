6605148431bd27a869197221055be39b6230a4f0
Jonathan M. Waldrop <relik107@gmail.com>
2024-08-20 18:46:51 -0500

    ubuntu clang fix, hopefully

--- .github/workflow_toolchains/clang-15.cmake
+++
@@ -1,6 +0,0 @@
-# Use whichever version of clang is the default for the OS
-set(CMAKE_C_COMPILER clang-15)
-set(CMAKE_CXX_COMPILER clang++-15)
-
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -stdlib=libc++")
-set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -stdlib=libc++ -lc++abi")
