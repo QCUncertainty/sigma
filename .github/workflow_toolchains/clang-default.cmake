ec7e6851f079b4bd4208f0d8a47ded6770b69923
Jonathan M. Waldrop <relik107@gmail.com>
2025-11-17 12:39:03 -0600

    GHA and CMake updates (#31)
    * bump default CMaize version to master
    
    * update pull_request workflow
    
    * Specify C++ standard in CMakeLists.txt
    
    * pre-commit action patch
    
    * PR actions workflow fix
    
    * Remove cxx standard flag from toolchains; revert linux clang test to Ubuntu 22.04

--- .github/workflow_toolchains/clang-default.cmake
+++ .github/workflow_toolchains/clang-default.cmake
@@ -1,4 +1,3 @@
-set(CMAKE_CXX_STANDARD 17)
 set(CMAKE_POSITION_INDEPENDENT_CODE TRUE)
 set(BUILD_SHARED_LIBS ON)
 set(CMAKE_BUILD_TYPE Debug)
@@ -10,5 +9,5 @@ set(CMAKE_C_COMPILER clang)
 set(CMAKE_CXX_COMPILER clang++)
 
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -stdlib=libc++")
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++17 -Wall -Werror")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Werror")
 set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -stdlib=libc++ -lc++abi")
