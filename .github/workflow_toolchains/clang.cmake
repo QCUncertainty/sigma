fff4cf54b84639adb62bc2bd3e201765e6d7a51b
Jonathan M. Waldrop <relik107@gmail.com>
2026-05-19 14:54:28 -0500

    try ccache action

--- .github/workflow_toolchains/clang.cmake
+++ .github/workflow_toolchains/clang.cmake
@@ -4,8 +4,10 @@ set(CMAKE_BUILD_TYPE Debug)
 set(BUILD_TESTING ON)
 set(BUILD_DOCS ON)
 
-# Use whichever version of clang is the default for the OS
 set(CMAKE_C_COMPILER clang)
 set(CMAKE_CXX_COMPILER clang++)
+
+set(CMAKE_C_COMPILER_LAUNCHER ccache)
+set(CMAKE_CXX_COMPILER_LAUNCHER ccache)
 
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Werror")
