c5359048776b300f5920d6ba53691fd75e24e848
Jonathan M. Waldrop <relik107@gmail.com>
2026-05-19 12:06:07 -0500

    Update the PR GHA workflow

--- .github/workflow_toolchains/clang.cmake
+++ .github/workflow_toolchains/clang.cmake
@@ -4,7 +4,8 @@ set(CMAKE_BUILD_TYPE Debug)
 set(BUILD_TESTING ON)
 set(BUILD_DOCS ON)
 
-set(CMAKE_C_COMPILER gcc-12)
-set(CMAKE_CXX_COMPILER g++-12)
+# Use whichever version of clang is the default for the OS
+set(CMAKE_C_COMPILER clang)
+set(CMAKE_CXX_COMPILER clang++)
 
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Werror")
